3d38d2bf2b260c09cfe9ece048f4e5e0c7268a7d
@OneJuicyDoor <shephardcam@gmail.com>
2023-05-17 08:27:56 -0300

    first commit

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("assertion Passed💚💚💚: " + actual + " === " + expected);
+  }
+  if (actual !== expected) {
+    console.log("assertion Failed🚩🚩🚩: " + actual + " !== " + expected);
+  }
+
+};
+
+//test
+assertEqual("lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("one", 1);
+assertEqual("beebs", "beebs");
