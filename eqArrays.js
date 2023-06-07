acc3fc4fb1f87701d8d712ccedef7aeac251bc05
@OneJuicyDoor <shephardcam@gmail.com>
2023-06-07 03:17:31 -0300

    updated test

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,4 @@
 
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`assertion Passed💚💚💚:  ${actual}  ===  ${expected}`);
-    }
-    if (actual !== expected) {
-      console.log(`assertion Failed🚩🚩🚩:  ${actual}  !==  ${expected}`);
-    }
-  
-  };
   function eqArrays(arr1, arr2) {
    if(arr1.length != arr2.length) {
     return false
@@ -21,11 +12,4 @@ const assertEqual = function(actual, expected) {
     return true;
   }
   
-  //test
-  assertEqual("lighthouse Labs", "Bootcamp");
-  assertEqual(1, 1);
-  assertEqual("one", 1);
-  assertEqual("beebs", "beebs");
-  assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-  assertEqual(eqArrays([5, 2, 3], [1, 2, 5]), true);
-  assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 6]), true);
+  module.exports = eqArrays;
