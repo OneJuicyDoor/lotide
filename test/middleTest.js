980668eb7dd2f979873d347c8986676a545ee2b4
@OneJuicyDoor <shephardcam@gmail.com>
2023-06-07 23:01:12 -0300

    added tests using mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,17 @@
-const middle = require('../middle')
+//const middle = require('../middle')
 
-middle([1,2,3,4,5,6])
-middle([1,2,3,4,5,6,7])
-middle([1])
+//middle([1,2,3,4,5,6])
+//middle([1,2,3,4,5,6,7])
+
+
+const assert = require('chai').assert;
+const theMiddle   = require('../middle');
+
+describe("#theMiddle", () => {
+  it("returns [3, 4] for [1,2,3,4,5,6]", () => {
+    assert.deepEqual(theMiddle([1,2,3,4,5,6]), [3, 4]);
+  });
+  it("returns 4 for [1,2,3,4,5,6,7]", () => {
+    assert.deepEqual(theMiddle([1,2,3,4,5,6,7]), [4]); 
+  });
+}); 
