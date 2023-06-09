3ebd3702dd6c76c4f3fbbbb249b22e0ff172ef80
@OneJuicyDoor <shephardcam@gmail.com>
2023-06-09 16:11:52 -0300

    made index work

--- tail.js
+++ tail.js
@@ -1,8 +1,9 @@
-const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
 
   const tail = function(array) {
     return array.slice(1);
   
     }
+assertArraysEqual(tail([5,6,7]), [6,7]);
 
     module.exports = tail;
